ba9195048851b1d6be8619a8f23a9ff781caec97
bhavyansh <bhavyansh2018@gmail.com>
2024-08-11 17:40:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/one_tap_google_signin/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.one_tap_google_signin
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.one_tap_google_signin.ui.theme.OneTapGoogleSignInTheme
+import com.stevdzasan.onetap.GoogleUser
+import com.stevdzasan.onetap.OneTapSignInWithGoogle
+import com.stevdzasan.onetap.getUserFromTokenId
+import com.stevdzasan.onetap.rememberOneTapSignInState
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            OneTapGoogleSignInTheme {
+                val state = rememberOneTapSignInState()
+                var user: GoogleUser? by remember { mutableStateOf(null) }
+                OneTapSignInWithGoogle(
+                    state = state,
+                    clientId = "YOUR CLIENT ID",
+                    rememberAccount = false,
+                    onTokenIdReceived = {
+                        user = getUserFromTokenId(tokenId = it)
+                        Log.d("MainActivity", user.toString())
+                    },
+                    onDialogDismissed = {
+                        Log.d("MainActivity", it)
+                    }
+                )
+
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Button(onClick = { state.open() }) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            if (state.opened) {
+                                CircularProgressIndicator(
+                                    color = Color.White
+                                )
+                            }
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(text = "Sign in")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
